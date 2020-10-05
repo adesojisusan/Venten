9c0c205f8c21df1d5ecb9eb0161fc040de7820d6
Susan Adesoji <adesojisusan.adeola@gmail.com>
2020-10-05 11:36:23 +0100

    test units

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,18 +2,3 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:venten/models/filter.dart';
 import 'package:venten/util.dart';
 
-void main() {
-  test('Should filter CSV file and print out filtered list', () async {
-    // Setup
-
-    Filter filterModel = Filter(
-      colors: ['Rose Gold'],
-    );
-
-    // Action
-    List carOwners = await Util.filterCsv(filterModel);
-
-    // Result
-    expect(carOwners.isEmpty, true);
-  });
-}
